e441c215be593a7b355998a4717379f063656be4
a.brunetkin <fxklim@gmail.com>
2014-08-01 18:44:59 +0500

    Билд объектов по сложным path с вложенными массивами

--- src/Morphine.js
+++ src/Morphine.js
@@ -56,18 +56,38 @@ function BuildObject (path, value) {
         intRegexp = /^[0-9]$/;
 
     for (var i = 0; i < props.length; i++) {
+        if (typeof iter === 'undefined') break;
+
         if (i == (props.length-1)) {
-            iter[props[i]] = value;
+            if (props[i] === '$') {
+                iter.push(value);
+            } else if (intRegexp.test(props[i])) {
+                if (typeof iter[props[i]] === 'undefined') {
+                    console.error("Элемент %o не существует", props.slice(0, i+1).join('.'));
+                }
+            } else {
+                iter[props[i]] = value;
+            }
         } else {
             if (intRegexp.test(props[i+1]) || props[i+1] === '$') {
                 // Если следующий элемент - число или текущий обозначен как коллекция, то на текущем уровне нужно создать массив
-                iter[props[i]] = (typeof iter[props[i]] !== 'undefined') ? iter[props[i]] : new MorphineArray();
+                if (props[i] === '$') {
+                    iter.push(new MorphineArray());
+                } else if (intRegexp.test(props[i])) {
+                    if (typeof iter[props[i]] === 'undefined') {
+                        console.error("Элемент %o не существует", props.slice(0, i+1).join('.'));
+                    }
+                } else {
+                    iter[props[i]] = (typeof iter[props[i]] !== 'undefined') ? iter[props[i]] : new MorphineArray();
+                }
             } else {
                 if (intRegexp.test(props[i])) {
                     if (typeof iter[props[i]] === 'undefined') {
                         // TODO: Неустоявшееся поведение: Если элемент еще не существует - ничего с ним не делаем, т.к. не ясно что с ним делать
+                        console.info("TODO: Неустоявшееся поведение: Если элемент еще не существует - ничего с ним не делаем, т.к. не ясно что с ним делать");
                     } else {
                         // TODO: Неустоявшееся поведение: Если элемент уже существует - ничего с ним не делаем, т.к. не ясно что с ним делать
+                        console.info("TODO: Неустоявшееся поведение: Если элемент уже существует - ничего с ним не делаем, т.к. не ясно что с ним делать");
                     }
                 } else if (props[i] === '$') {
                     iter.push(new Morphine());
