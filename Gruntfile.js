4fae6f9e92fe8a68da5782691aea3baa283b8f5a
a.brunetkin <fxklim@gmail.com>
2014-08-05 18:25:06 +0500

    Добавлены тесты qunit

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,9 @@ module.exports = function(grunt) {
                 src: 'build/<%= pkg.name %>.<%= pkg.version %>.js',
                 dest: 'build/<%= pkg.name %>.<%= pkg.version %>.min.js'
             }
+        },
+        qunit: {
+            files: ['tests/index.html']
         }
     });
 
@@ -35,7 +38,12 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+
+    // grunt-contrib-qunit
     // Default task(s).
     grunt.registerTask('default', ['concat', 'uglify']);
+
+    grunt.registerTask('test', 'qunit');
 
 };
---
+++ tests/test.js
@@ -0,0 +1,11 @@
+
+
+QUnit.test( "assert ok 1 === 1", function( assert ) {
+    assert.ok( 1 == "1", "Passed!" );
+});
+
+QUnit.test( "assert ok 1 === 2", function( assert ) {
+    assert.ok( 1 == "2", "Passed!" );
+});
+
+
