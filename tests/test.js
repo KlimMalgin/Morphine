1315a074e54d3892902a9a3ef67b7df55dc7eaef
KlimMalgin <fxklim@gmail.com>
2014-10-20 01:03:07 +0500

    Добавлены простейшие тесты для checkType

--- tests/test.js
+++ tests/test.js
@@ -9,3 +9,17 @@ describe('Common tests', function () {
         assert.equal(m.isObject(), true, 'Is Object');
     });
 });
+
+
+describe('checkType()', function () {
+    it('Стандартная проверка элементарных типов', function () {
+        var checkType = MorphineShareApi.checkType;
+        var bool = true,
+            int = 24,
+            str = "";
+
+        assert.equal(checkType(bool), true, 'Переданное в checkType значение является элементарным типом Boolean');
+        assert.equal(checkType(int), true, 'Переданное в checkType значение является элементарным типом Number');
+        assert.equal(checkType(str), true, 'Переданное в checkType значение является элементарным типом String');
+    });
+});
